0a5a91fa24d5ae4f75b6dbc49f3d3bb22934b7f0
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-24 09:36:55 -0300

    feat(models): create Category enum

---
+++ src/main/java/com/example/retornosAPI/models/Category.java
@@ -0,0 +1,51 @@
+package com.example.retornosAPI.models;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public enum Category {
+    ACCESSORIES("Acessórios"),
+    APPLIANCES("Eletrodomésticos"),
+    AUTOMOTIVE("Automotivo"),
+    BEAUTY("Beleza"),
+    BOOKS("Livros"),
+    CLEANING("Limpeza"),
+    CLOTHING("Roupas"),
+    CONSTRUCTION("Construção"),
+    DECORATION("Decoração"),
+    DRINKS("Bebidas"),
+    ELECTRONICS("Eletrônicos"),
+    FOOD("Alimentos"),
+    FURNITURE("Móveis"),
+    GAMES("Jogos"),
+    HEALTH("Saúde"),
+    HYGIENE("Higiene"),
+    IT("Informática"),
+    LEISURE("Lazer"),
+    MOVIES("Filmes"),
+    MUSIC("Músicas"),
+    SPORTS("Esportes"),
+    STATIONERY("Papelaria"),
+    TOOLS("Ferramentas"),
+    TOYS("Brinquedos"),
+    TRAVEL("Viagem"),
+    UTILITIES("Utilidades");
+
+    private final String description;
+
+    Category(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Optional<Category> validateCategory(String description) {
+        return Stream.of(Category.values())
+                .filter(
+                        category -> getDescription()
+                                .equalsIgnoreCase(description)
+                ).findAny();
+    }
+}
